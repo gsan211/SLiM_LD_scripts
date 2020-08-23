d20dcab6675c9297cb55b05faa9dac9b1c679a82
gsan211 <36670832+gsan211@users.noreply.github.com>
2020-08-23 11:59:46 -0400

    Update run_slim.sh

--- run_slim.sh
+++ run_slim.sh
@@ -8,5 +8,5 @@ directory = "/plas1/george.sandler/syn_epi/equal_pops/slim_subpops_nov.txt"
 
 
 cd directory
-parallel --jobs 20 /plas1/apps/SLiM.2.4.2/bin/slim -d o={} "/plas1/george.sandler/syn_epi/slim_spatial_seln.txt"  ::: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+parallel --jobs 20 /plas1/apps/SLiM.2.4.2/bin/slim -d o={} slim_file  ::: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 
